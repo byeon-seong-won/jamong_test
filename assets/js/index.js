1e80eb979075c590806b6815c17cadfc00095e4c
byeon-seong-won <sssingw@naver.com>
2024-09-02 11:50:29 +0900

    수정

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,15 +10,15 @@
 
 
     // --------------- 전체 부드러운 스크롤 --------------- 
-    const lenis = new Lenis()
-    lenis.on('scroll', (e) => {
-      console.log(e)
-    })
-    lenis.on('scroll', ScrollTrigger.update)
-    gsap.ticker.add((time)=>{
-      lenis.raf(time * 700)
-    })
-    gsap.ticker.lagSmoothing(0)
+    // const lenis = new Lenis()
+    // lenis.on('scroll', (e) => {
+    //   console.log(e)
+    // })
+    // lenis.on('scroll', ScrollTrigger.update)
+    // gsap.ticker.add((time)=>{
+    //   lenis.raf(time * 700)
+    // })
+    // gsap.ticker.lagSmoothing(0)
 
 
 
